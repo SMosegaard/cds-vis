202d802ccebb6fbb414d7165cffe31cbb7ea2aa7
SMosegaard <sofie.mosegaard@live.dk>
2024-03-25 12:08:44 +0100

    updated assignment after feedback

--- assignments/assignment-1/src/src.py
+++ assignments/assignment-1/src/src.py
@@ -10,17 +10,22 @@ import matplotlib.pyplot as plt
 def read_image(filepath):
     return cv2.imread(filepath)
 
+
 def calculate_histogram(image):
     return cv2.calcHist([image], [0, 1, 2], None, [255, 255, 255], [0, 256, 0, 256, 0, 256])
+
 
 def normalize_histogram(hist):
     return cv2.normalize(hist, hist, 0, 1.0, cv2.NORM_MINMAX)
 
+
 def update_distance_df(distance_df, filename, distance):
     distance_df.loc[len(distance_df.index)] = [filename, distance]
 
+
 def compare_histograms(hist1, hist2):
     return round(cv2.compareHist(hist1, hist2, cv2.HISTCMP_CHISQR), 3)
+
 
 def save_dataframe_to_csv(distance_df, csv_outpath):
     distance_df.to_csv(csv_outpath)
@@ -29,30 +34,19 @@ def save_dataframe_to_csv(distance_df, csv_outpath):
 def process_images(filepath_f1, filepath):
 
     """
-    The process_images function will ...
+    The process_images function systematically analyzes images by extracting and comparing normalized histograms.
+    Initially, a normalized histogram encompassing all color channels is extracted from the target image (f1).
 
-    Initially, one normalised histogram of all color channels will be extracted from one particular image.
+    Subsequently, a pandas dataframe is initialized to store image filenames and their corresponding distance. As
+    the function iterates through all images in a sorted order, each image undergoes histogram calculation and
+    normalization. The resulting normalized histogram will then be compared to that of the target image.
 
-    Afterwards, a pandas dataframe with specified column names will be initialized. 
-
-    Loop through all images in sorted order.
-
-    In the loop, the current image will gain a calculated histogram and normalize it.
-    The norm hist of the current image will be compared to image 1. 
-
-    As the dataframe will end up having five images which are most simlar to the target image, the function
-    will first append the image name and distance if there is less than 6 filled rows in the dataframe.
-    Otherwise, it will find the image with the highest distance (so the lest similar image to the target)
-    in the dataframe.
- 
-        First, I want to append the first 5 images' names and dist values to the distance_df. When the table
-        consitsts of five images, I want to compare the distance between the image with the biggest dist value
-        in the distance_df ( = so the image least similar to the target image (image_f1)) and the dist value
-        of the current image. If the current image has a smaller dist, I want to update the df.
-
-
+    The dataframe aims to hold the five most similar images to the target image. If the dataframe contains
+    fewer than six entries, the function appends the current image's filename and distance. Otherwise, it
+    finds the image with the highest distance (i.e., the least similar to the target) and assesses whether the
+    current image has a smaller distance. If so, the dataframe is updated accordingly.
     """
-    
+
     image_f1 = read_image(filepath_f1)
     hist_f1 = calculate_histogram(image_f1)
     norm_hist_f1 = normalize_histogram(hist_f1)
@@ -84,12 +78,10 @@ def process_images(filepath_f1, filepath):
     return distance_df
 
 
-
-
 def main():
 
-    filepath_f1 = os.path.join("..", "..", "..", "..", "cds-vis-data", "flowers", "image_0001.jpg")
-    filepath = os.path.join("..", "..", "..", "..", "cds-vis-data", "flowers")
+    filepath_f1 = os.path.join("in", "image_0001.jpg")
+    filepath = os.path.join("in")
 
     distance_df = process_images(filepath_f1, filepath)
 
