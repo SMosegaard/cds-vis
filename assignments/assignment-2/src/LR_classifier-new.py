830077383aec7aa5f4a2c8d202239c65ab85db84
SMosegaard <sofie.mosegaard@live.dk>
2024-03-29 12:53:50 +0100

    commit changes after feedback

--- assignments/assignment-2/src/LR_classifier-new.py
+++ assignments/assignment-2/src/LR_classifier-new.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import ShuffleSplit, GridSearchCV, learning_curve
 from sklearn import metrics
 import matplotlib.pyplot as plt
 import tensorflow
 from tensorflow.keras.datasets import cifar10
+
 
 # Convert images to greyscale
 def greyscale(X):
@@ -51,47 +53,67 @@ def preprocess_data(X_train, X_test):
     return X_train_scaled_reshape, X_test_scaled_reshape
 
 
-
-
-# Function that defines and fits the neural netork classifier to the data
 def define_and_fit_classifier(X_train_scaled_reshape, y_train):
     
-    classifier = LogisticRegression(tol = 0.1,
-                                    solver = 'saga',
+    """
+    Function that defines and fits the neural netork classifier to the data 
+    """
+
+    classifier = LogisticRegression(solver = 'saga',
                                     multi_class = 'multinomial',
                                     random_state = 123,
                                     verbose = True)
 
-    LR_classifier = classifier.fit(X_train_scaled_reshape, y_train)
+    classifier = classifier.fit(X_train_scaled_reshape, y_train)
 
-    return LR_classifier
+    return classifier
 
 
-# Function that evaluates the trained classifier on new, unseen data
-def evaluate_classifier(LR_classifier, X_train_scaled_reshape, y_train, X_test_scaled_reshape,  y_test):
-    y_pred = LR_classifier.predict(X_test_scaled_reshape)     # Generate predictions
+def grid_search(classifier, X_train, y_train):
+
+    param_grid = {'tol': [0.01, 0.1, 1], 'max_iter': [100, 200, 300]}
+
+    grid_search = GridSearchCV(estimator = classifier, param_grid = param_grid, cv = 5, n_jobs = -1)
+    grid_result = grid_search.fit(X_train, y_train)
+
+    print(f'Best Accuracy for {grid_result.best_score_} using the parameters {grid_result.best_params_}')
+
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print(f' mean={mean:.4}, std={stdev:.4} using {param}')
+
+    return grid_result.best_estimator_
+
+
+def evaluate_classifier(classifier, X_train_scaled_reshape, y_train, X_test_scaled_reshape,  y_test):
+
+    """
+    Function that evaluates the trained classifier on new, unseen data. This includes plotting a confusion
+    matrix and calculating a classification report, which will be saved.
+    """
+    y_pred = classifier.predict(X_test_scaled_reshape) 
             
-    # Plot confusion matrix
-    metrics.ConfusionMatrixDisplay.from_estimator(LR_classifier,
+    metrics.ConfusionMatrixDisplay.from_estimator(classifier,
                                                 X_train_scaled_reshape,
                                                 y_train,
                                                 cmap = plt.cm.Blues)
 
-    # Change labels from numbers to object names
     labels = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
 
-    # Calculate classification report
     classifier_metrics = metrics.classification_report(y_test, y_pred, target_names = labels)
     print(classifier_metrics)
 
-    # Save classification report
     filepath_report = "../out/LR_classification_report.txt"
     with open(filepath_report, 'w') as file:
         file.write(classifier_metrics)
 
 
-# Function that executes all the functions above in a structered manner on the CIFAR-10 dataset
+
+# Main function that executes all the functions above in a structered manner on the CIFAR-10 dataset
 def main():
+
     # Load CIFAR-10 dataset
     (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
@@ -101,8 +123,12 @@ def main():
     # Define and fit classifier
     LR_classifier = define_and_fit_classifier(X_train_scaled_reshape, y_train)
 
+    # Grid search
+    best_LR_classifier = grid_search(LR_classifier, X_train_scaled_reshape, y_train)
+
     # Evaluate classifier
-    evaluate_classifier(LR_classifier, X_train_scaled_reshape, y_train, X_test_scaled_reshape, y_test)
+    evaluate_classifier(best_LR_classifier, X_train_scaled_reshape, y_train, X_test_scaled_reshape, y_test)
+
 
 if __name__ == "__main__":
     main()
