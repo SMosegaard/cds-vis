12da67d7d6cf0cdc588c2df6af0e291d7fed2bf7
SMosegaard <sofie.mosegaard@live.dk>
2024-04-11 11:33:30 +0200

    updated bash script

--- assignments/assignment-3/run.sh
+++ assignments/assignment-3/run.sh
@@ -1,7 +1,14 @@
 # Activate the envoriment
 source ./env/bin/activate 
-# Run the code
-python src/src.py
+
+# Run the pretrained CNN
+python src/TransferLearning.py "$@"
+
+# Run the pretrained CNN with batch normalization
+python src/TransferLearning_BatchNorm.py "$@"
+
+# Run the pretrained CNN with data augmentation
+python src/TransferLearning_DatAug.py "$@"
 
 # Close the envoriment
 deactivate
