0ce6e9d59c127548b74878078506e901852a0fcc
SMosegaard <sofie.mosegaard@live.dk>
2024-04-19 15:54:13 +0200

    trying to implement VGG16 and K-NN, not done yet

--- assignments/assignment-1/src/src.py
+++ assignments/assignment-1/src/src.py
@@ -1,10 +1,31 @@
 import pandas as pd
-import os
+import os, sys
 import cv2
 import numpy as np
 from utils.imutils import jimshow as show
 from utils.imutils import jimshow_channel as show_channel
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from numpy.linalg import norm
+from tqdm import notebook
+import tensorflow_hub as hub
+from tensorflow.keras.preprocessing.image import (load_img, img_to_array)
+from tensorflow.keras.applications.vgg16 import (VGG16, preprocess_input)
+from sklearn.neighbors import NearestNeighbors
+
+
+def parser():
+    """
+    The user can specify whether to perform image search using color histograms or a pretrained CNN VGG16 model
+    and K-Nearst Neighbour
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--method",
+                        "-m",
+                        required = True,
+                        help = "Color histogram or VGG16?")
+    args = parser.parse_args()
+    return args
 
 
 def read_image(filepath):
@@ -78,17 +99,109 @@ def process_images(filepath_f1, filepath):
     return distance_df
 
 
+def extract_features_input(filepath, model):
+    """
+    Extract features from image input using the pretrained VGG16 model
+    """
+    input_shape = (224, 224, 3)
+    img = load_img(filepath, target_size = (input_shape[0], input_shape[1]))
+    img_array = img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    features = model.predict(preprocessed_img, verbose = False)
+    flattened_features = features.flatten()
+    normalized_features = flattened_features / norm(features)
+    return flattened_features
+
+
+def pretrained_model():
+    """
+    Load pretrained VGG16 model
+    """
+    model = VGG16(weights = 'imagenet',
+                include_top = False,          
+                pooling = 'avg',
+                input_shape = (224, 224, 3))
+    return model
+
+def load_classifier():
+    """
+    Load the K-Nearst Neighbour classification model
+    """
+    neighbors = NearestNeighbors(n_neighbors = 10,
+                             algorithm = 'brute',
+                             metric = 'cosine').fit(feature_list)
+    return neighbors, feature_list
+
+
+def extract_features_image(model, feature_list, filenames):
+    feature_list = []
+    for i in notebook.tqdm(range(len(filenames))):
+        feature_list.append(extract_features(filenames[i], model))
+    return feature_list
+
+
+def calculate_nn(model, feature_list, target_image):
+    """
+    Calculate the nearest neighbours for target image
+    """
+    distances, indices = neighbors.kneighbors([feature_list[target_image]])
+    return distances, indices
+
+
+def save_indices(distances, indices):
+    idx = []
+    for i in range(1, 6):
+        distances[0][i], indices[0][i]
+        idx.append(indices[0][i])
+    return idx
+
+
+def plot_target_vs_closest(idx, filenames, target_image):
+    
+    plt.imshow(mpimg.imread(filenames[target_image]))
+    f, axarr = plt.subplots(1,5)
+    axarr[0].imshow(mpimg.imread(filenames[idxs[0]]))
+    axarr[1].imshow(mpimg.imread(filenames[idxs[1]]))
+    axarr[2].imshow(mpimg.imread(filenames[idxs[2]]))
+    axarr[3].imshow(mpimg.imread(filenames[idxs[3]]))
+    axarr[4].imshow(mpimg.imread(filenames[idxs[4]]))
+
+
+
 def main():
 
-    filepath_f1 = os.path.join("in", "image_0001.jpg")
-    filepath = os.path.join("in")
+    args = parser()
 
-    distance_df = process_images(filepath_f1, filepath)
+    if args.method.lower() == 'histogram':
 
-    print(distance_df)
+        filepath_f1 = os.path.join("in", "image_0001.jpg")
+        filepath = os.path.join("in")
 
-    csv_outpath = os.path.join("out", "output.csv")
-    save_dataframe_to_csv(distance_df, csv_outpath)
+        distance_df = process_images(filepath_f1, filepath)
+
+        print(distance_df)
+
+        csv_outpath = os.path.join("out", "output.csv")
+        save_dataframe_to_csv(distance_df, csv_outpath)
+
+    else:
+        
+        model = pretrained_model()
+
+        target_image = os.path.join("..", "..", "..", "cds-vis-data", "flowers") # "in", "image_0001.jpg"
+        features = extract_features_input(target_image, model)
+
+        root_dir = os.path.join("..", "..", "..", "cds-vis-data", "flowers") # # "in", "image_0001.jpg
+        filenames = [root_dir + "/" + name for name in sorted(os.listdir(root_dir))]
+
+        feature_list = extract_features_image(model, feature_list, filenames)
+
+        neighbors, feature_list = load_classifier()
+        distances, indices = calculate_nn(neighbors, feature_list, target_image)
+
+        idx = save_indices(distances, indices)
+        plot_target_vs_closest(idx, filenames, target_image)
 
 if __name__ == "__main__":
     main()
