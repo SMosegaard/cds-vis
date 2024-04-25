0ff72b2a75e2c39f5bb64a7a3c7d84e221f5b612
SMosegaard <sofie.mosegaard@live.dk>
2024-04-25 17:58:52 +0200

    created setup bash script

---
+++ assignments/assignment-4/setup.sh
@@ -0,0 +1,12 @@
+# Create virtual envoriment called env
+python -m venv env
+
+# Activate the virtual env
+source ./env/bin/activate
+
+# Install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Close the envoriment
+deactivate
