346997c2ed0314ac75a1b2c7995d97477405388a
SMosegaard <sofie.mosegaard@live.dk>
2024-04-30 12:06:42 +0200

    changed order

--- assignments/assignment-1/setup.sh
+++ assignments/assignment-1/setup.sh
@@ -4,14 +4,13 @@ python -m venv env
 # Activate the virtual env
 source ./env/bin/activate
 
+# Install opencv 
+sudo apt-get update
+sudo apt-get install -y python3-opencv
+
 # Install requirements
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Install opencv
-sudo apt-get update
-sudo apt-get install -y python3-opencv
-pip install opencv-python matplotlib
 
 # Close the envoriment
 deactivate
