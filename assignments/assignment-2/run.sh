cf40be7f1d57161dfd341ed6e1fd814fb53d1179
SMosegaard <sofie.mosegaard@live.dk>
2024-05-03 18:00:55 +0200

    updated

--- assignments/assignment-2/run.sh
+++ assignments/assignment-2/run.sh
@@ -2,7 +2,7 @@
 source ./env/bin/activate 
 
 # Run the LR classifier
-python src/LR_classifier.py "$@"
+#python src/LR_classifier.py "$@"
 
 # Run the NN classifier
 python src/NN_classifier.py "$@"
