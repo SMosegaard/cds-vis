d5dfc08281568a6b52cc5589ae03f7907f8d3011
SMosegaard <sofie.mosegaard@live.dk>
2024-05-08 13:03:13 +0200

    did more on combining the scripts + gridsearch

--- assignments/assignment-3/src/gridsearch.py
+++ assignments/assignment-3/src/gridsearch.py
@@ -1,19 +1,16 @@
 
 def grid_search(model, X_train, y_train):
-
-
+    
     param_grid = {'optimizer' = ['sgd', 'adam'],
                 'learning_rate' = [0.1, 0.01, 0.001, 0.0001],
                 'epochs' = [10, 15, 20],
                 'batch_size' = [16, 32, 64]}
-
 
     grid_search = GridSearchCV(estimator = model, param_grid = param_grid, cv = 5, n_jobs = -1,
                                 scoring = 'accuracy', verbose = 3)
 
     grid_result = grid_search.fit(X_train, y_train)
     grid_result = grid_search.fit_model(X_train, y_train)
-
 
     print(f'Best Accuracy for {grid_result.best_score_} using the parameters {grid_result.best_params_}')
 
@@ -26,4 +23,3 @@ def grid_search(model, X_train, y_train):
     best_estimator = grid_result.best_estimator_
 
     return best_estimator
-
