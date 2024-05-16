1b8e0389e2fb6a9d9affe89c5c14a37e4f7c0406
SMosegaard <sofie.mosegaard@live.dk>
2024-05-16 10:01:57 +0200

    used = instead of : in param grid

--- assignments/assignment-3/src/gridsearch.py
+++ assignments/assignment-3/src/gridsearch.py
@@ -1,16 +1,15 @@
 
 def grid_search(model, X_train, y_train):
     
-    param_grid = {'optimizer' = ['sgd', 'adam'],
-                'learning_rate' = [0.1, 0.01, 0.001, 0.0001],
-                'epochs' = [10, 15, 20],
-                'batch_size' = [16, 32, 64]}
+    param_grid = {'optimizer': ['sgd', 'adam'],
+                'learning_rate': [0.1, 0.01, 0.001, 0.0001],
+                'epochs': [10, 15, 20],
+                'batch_size': [16, 32, 64]}
 
     grid_search = GridSearchCV(estimator = model, param_grid = param_grid, cv = 5, n_jobs = -1,
-                                scoring = 'accuracy', verbose = 3)
+                                scoring = 'accuracy', verbose = 1)
 
     grid_result = grid_search.fit(X_train, y_train)
-    grid_result = grid_search.fit_model(X_train, y_train)
 
     print(f'Best Accuracy for {grid_result.best_score_} using the parameters {grid_result.best_params_}')
 
@@ -18,7 +17,7 @@ def grid_search(model, X_train, y_train):
     stds = grid_result.cv_results_['std_test_score']
     params = grid_result.cv_results_['params']
     for mean, stdev, param in zip(means, stds, params):
-        print(f' mean={mean:.4}, std={stdev:.4} using {param}')
+        print(f' mean = {mean:.4}, std = {stdev:.4} using {param}')
 
     best_estimator = grid_result.best_estimator_
 
