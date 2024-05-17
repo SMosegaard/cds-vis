e7296d038813cb8bf13fb0293448be75ac5ea566
SMosegaard <sofie.mosegaard@live.dk>
2024-05-17 11:24:52 +0200

    now that I have a combined py script, i deleted the 3 runs

--- assignments/assignment-3/run.sh
+++ assignments/assignment-3/run.sh
@@ -1,14 +1,8 @@
 # Activate the envoriment
 source ./env/bin/activate 
 
-# Run the pretrained CNN
-python src/TransferLearning.py "$@"
-
-# Run the pretrained CNN with batch normalization
-python src/TransferLearning_BatchNorm.py "$@"
-
-# Run the pretrained CNN with data augmentation
-python src/TransferLearning_DatAug.py "$@"
+# Run the code
+python src/document_classifier.py "$@"
 
 # Close the envoriment
 deactivate
